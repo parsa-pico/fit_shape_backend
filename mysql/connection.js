e7e445376548e2acc480f8796b5b831cacf4bb45
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-01-23 12:24:14 +0330

    #

--- mysql/connection.js
+++ mysql/connection.js
@@ -1,10 +1,10 @@
-const mysql2 = require('mysql2');
+const mysql2 = require("mysql2");
 
 const pool = mysql2.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'fit_shape',
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "fit_shape",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
