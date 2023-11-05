62b637c234373f8ccd944857012fbe6c190597e0
Parsa Foroozmand <parsaforoozmand@gmail.com>
2023-11-05 20:23:16 +0330

    #

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,22 +1,23 @@
-const winston = require('winston');
+const winston = require("winston");
 
 module.exports = function (err, req, res, next) {
   const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     format: winston.format.combine(
       winston.format.timestamp(),
       winston.format.json()
     ),
-    defaultMeta: { service: 'user-service' },
+    defaultMeta: { service: "user-service" },
     transports: [
       //
       // - Write all logs with importance level of `error` or less to `error.log`
       // - Write all logs with importance level of `info` or less to `combined.log`
       //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+      new winston.transports.File({ filename: "error.log", level: "error" }),
+      new winston.transports.File({ filename: "combined.log" }),
     ],
   });
+  console.log(err.message, err);
   logger.error(err.message, err);
-  res.status(500).send('something failed in the server');
+  res.status(500).send("something failed in the server");
 };
